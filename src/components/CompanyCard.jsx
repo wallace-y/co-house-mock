cf7c3863e51b54a7aae05170c9b28f26b60947fa
wallace-y <cameronwfinney@gmail.com>
2023-02-17 11:56:07 +0000

    updating card structure and adding links

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const CompanyCard = ({ company: { company_number,company_status,date_of_creation,title } }) => {
   return (
-    <div className="company" key={company_number}>
-      <div>
-        <h3>{title}</h3>
+    <div className="company card" key={company_number}>
+      <div class="card-body">
+          <h5 class="card-title">{title}</h5>
+          <p class="card-text">Company status: {company_status}</p>
+          <p class="card-text">Company number: {company_number}</p>
+          <a href={"https://find-and-update.company-information.service.gov.uk/company/"+company_number}
+              class="btn btn-dark">Go somewhere</a>
       </div>
     </div>
+  
   );
 }
 
