6646eb5dae0c2a04ca8dbde2b7c0326d53e44507
wallace-y <cameronwfinney@gmail.com>
2023-02-17 12:48:03 +0000

    card sizing

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Row,Col } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 const CompanyCard = ({ company: { company_number,company_status,date_of_creation,title } }) => {
   return (
-    <Col sm={12} md={6} lg={2} className="company card m-5" key={company_number}>
+    <Col sm={12} md={6} lg={3} className="company card m-5" key={company_number}>
       <div class="card-body">
           <h5 class="card-title">{title}</h5>
           <p class="card-text">Company status: {company_status}</p>
