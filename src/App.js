8fb419e84516ed13feb45edf85d705709a4a1669
wallace-y <cameronwfinney@gmail.com>
2023-02-19 13:06:37 +0000

    adding proxy server note

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ const App = () => {
                 <h1 class="display-5 fw-bold">Company Search</h1>
                 <div class="col-lg-6 mx-auto">
                         <p class="lead mb-4">Search for any real-life company from the live register of companies from Companies House.</p>
+                        <p>Please click <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank">here</a> to enable the CORS proxy before searching.</p>
                     <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
                     <input class="form-control rounded"
                 placeholder="Search for companies"
